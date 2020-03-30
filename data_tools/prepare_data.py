8f14e304b14d62105160ee8a6d302f192e67e782
arpanmangal <mangalarpan@gmail.com>
2020-03-30 23:09:29 +0530

    Code for combined dataset preparation

--- data_tools/prepare_data.py
+++ data_tools/prepare_data.py
@@ -1,34 +1,47 @@
 """
-Script to prepare data
+Script to prepare combined dataset
+Class 0: Normal
+Class 1: Bacterial Pneumonia
+Class 2: Viral Pneumonia
+Class 3: COVID-19
 """
 import glob
 import os
 
-covid_path = "../covid19_xray"
-pneumonia_path = "../chest-xray-pneumonia"
+COVID19_DATA_PATH = "./data/covid19"
+COVID19_IMGS_PATH = "./covid-chestxray-dataset/images"
+PNEUMONIDA_DATA_PATH = "./chest-xray-pneumonia"
+DATA_PATH = "./data"
+
+# Assert that the data directories are present
+for d in [COVID19_DATA_PATH, COVID19_IMGS_PATH, PNEUMONIDA_DATA_PATH, DATA_PATH]:
+    try:
+        assert os.path.isdir(d) 
+    except:
+        print ("Directory %s does not exists" % d)
 
 def create_list (split):
     assert split in ['train', 'test', 'val']
 
     l = []
     # Prepare list using kaggle pneumonia dataset
-    for f in glob.glob(os.path.join(pneumonia_path, split, 'NORMAL', '*')):
+    for f in glob.glob(os.path.join(PNEUMONIDA_DATA_PATH, split, 'NORMAL', '*')):
         l.append((f, 0)) # Class 0
 
-    for f in glob.glob(os.path.join(pneumonia_path, split, 'PNEUMONIA', '*')):
+    for f in glob.glob(os.path.join(PNEUMONIDA_DATA_PATH, split, 'PNEUMONIA', '*')):
         if 'bacteria' in f:
             l.append((f, 1)) # Class 1
         else:
             l.append((f, 2)) # Class 2
 
     # Prepare list using covid dataset
-    covid_file = os.path.join(covid_path, '%s.txt'%split)
+    covid_file = os.path.join(COVID19_DATA_PATH, '%s_list.txt'%split)
     with open(covid_file, 'r') as cf:
         for f in cf.readlines():
-            f = os.path.join(covid_path, 'xray_covid_images', f.strip())
+            f = os.path.join(COVID19_IMGS_PATH, f.strip())
             l.append((f, 3)) # Class 3
 
-    with open('data/%s.txt'%split, 'w') as f:
+    with open(os.path.join(DATA_PATH, '%s.txt'%split), 'w') as f:
         for item in l:
             f.write("%s %d\n" % item)
 
