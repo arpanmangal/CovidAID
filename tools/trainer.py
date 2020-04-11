3d5f52be6e292a0d052ca276c6443ec5d125a8c9
arpanmangal <mangalarpan@gmail.com>
2020-04-11 18:21:17 +0530

    Code for ROC curve

--- tools/trainer.py
+++ tools/trainer.py
@@ -15,9 +15,11 @@ from torch.utils.data.distributed import DistributedSampler
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 from read_data import ChestXrayDataSet, load_single_image
-from sklearn.metrics import roc_auc_score, confusion_matrix
+from sklearn.metrics import roc_auc_score, confusion_matrix, roc_curve, auc
 import seaborn as sn
 import pandas as pd
+from scipy import interp
+from itertools import cycle
 import matplotlib
 import matplotlib.pyplot as plt
 from covidxnet import CovidXNet
@@ -220,7 +222,7 @@ class Trainer:
 
         print (pred)
 
-    def predict(self, TEST_IMAGE_LIST, BATCH_SIZE=64, cm_path='cm'):
+    def predict(self, TEST_IMAGE_LIST, BATCH_SIZE=64, cm_path='cm', roc_path='roc'):
         """
         Predict the task labels corresponding to the input images
         """
@@ -280,6 +282,9 @@ class Trainer:
             labels = ['Normal', 'Pneumonia', 'COVID-19']
         self.compute_AUC_scores(gt, pred, labels)
 
+        # Plot ROC scores
+        self.plot_ROC_curve(gt, pred, labels, roc_path)
+
         # Treat the max. output as prediction. 
         # Plot Confusion Matrix
         gt = gt.argmax(axis=1)
@@ -304,7 +309,6 @@ class Trainer:
             [("%d (%.2f)" % (c, nc)) for c, nc in zip(r, nr)]
             for r, nr in zip(cm, norm_cm)
         ]
-        # df_cm = pd.DataFrame(cm, index=labels, columns=labels).astype(int)
         plt.figure(figsize = (10,7))
         sn.heatmap(norm_df_cm, annot=annot, fmt='', cbar=False, square=True, cmap=plt.cm.Blues)
         plt.xlabel("Predicted")
@@ -329,6 +333,73 @@ class Trainer:
         for y, pred, label in zip(y_true.transpose(), y_pred.transpose(), labels):
             print('The AUROC of {0:} is {1:.4f}'.format(label, roc_auc_score(y, pred)))
 
+    def plot_ROC_curve(self, y_true, y_pred, labels, roc_path): 
+        """
+        Plots the ROC curve from prediction scores
+
+        y_true.shape  = [n_samples, n_classes]
+        y_preds.shape = [n_samples, n_classes]
+        labels.shape  = [n_classes]
+        """
+        n_classes = len(labels)
+        # Compute ROC curve and ROC area for each class
+        fpr = dict()
+        tpr = dict()
+        roc_auc = dict()
+        for y, pred, label in zip(y_true.transpose(), y_pred.transpose(), labels):
+            fpr[label], tpr[label], _ = roc_curve(y, pred)
+            roc_auc[label] = auc(fpr[label], tpr[label])
+
+        # First aggregate all false positive rates
+        all_fpr = np.unique(np.concatenate([fpr[label] for label in labels]))
+
+        # Then interpolate all ROC curves at this points
+        mean_tpr = np.zeros_like(all_fpr)
+        for label in labels:
+            mean_tpr += interp(all_fpr, fpr[label], tpr[label])
+
+        # Finally average it and compute AUC
+        mean_tpr /= n_classes
+
+        # Compute micro-average ROC curve and ROC area
+        fpr["micro"], tpr["micro"], _ = roc_curve(y_true.ravel(), y_pred.ravel())
+        roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+        fpr["macro"] = all_fpr
+        tpr["macro"] = mean_tpr
+        roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+        # Plot all ROC curves
+        plt.figure()
+        lw = 2
+        plt.plot(fpr["micro"], tpr["micro"],
+                label='micro-average ROC curve (area = {0:0.3f})'
+                    ''.format(roc_auc["micro"]),
+                color='deeppink', linestyle=':', linewidth=2)
+
+        plt.plot(fpr["macro"], tpr["macro"],
+                label='macro-average ROC curve (area = {0:0.3f})'
+                    ''.format(roc_auc["macro"]),
+                color='navy', linestyle=':', linewidth=2)
+
+        if len(labels) == 4:
+            colors = ['green', 'cornflowerblue', 'darkorange', 'darkred']
+        else:
+            colors = ['green', 'cornflowerblue', 'darkred']
+        for label, color in zip(labels, cycle(colors)):
+            plt.plot(fpr[label], tpr[label], color=color, lw=lw,
+                    label='ROC curve of {0} (area = {1:0.3f})'
+                    ''.format(label, roc_auc[label]))
+
+        plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('ROC curve')
+        plt.legend(loc="lower right")
+        plt.savefig('%s.png' % roc_path, pad_inches = 0, bbox_inches='tight')
+
     def save_model(self, checkpoint_path, model=None):
         if model is None: model = self.net
         torch.save(model.state_dict(), checkpoint_path)
@@ -352,6 +423,8 @@ if __name__ == '__main__':
     parser.add_argument("--bs", type=int, default=8)
     parser.add_argument("--inc_recall", type=int, default=None)
     parser.add_argument("--cm_path", type=str, default='plots/cm')
+    parser.add_argument("--roc_path", type=str, default='plots/roc')
+
     # parser.add_argment("--torch_version", "--tv", choices=["0.3", "new"], default="0.3")
     args = parser.parse_args()
 
@@ -365,7 +438,7 @@ if __name__ == '__main__':
 
     trainer = Trainer(local_rank=args.local_rank, checkpoint=args.checkpoint, combine_pneumonia=args.combine_pneumonia)
     if args.mode == 'test':
-        trainer.predict(TEST_IMAGE_LIST, cm_path=args.cm_path)
+        trainer.predict(TEST_IMAGE_LIST, cm_path=args.cm_path, roc_path=args.roc_path)
     else:
         assert args.save is not None
         trainer.train(TRAIN_IMAGE_LIST, VAL_IMAGE_LIST, BATCH_SIZE=args.bs, NUM_EPOCHS=300, LR=args.lr,
